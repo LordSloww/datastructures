5f2ef9e9864dd3d6680a8b6338075927b807c09c
Lord Slow <151541925+LordSloww@users.noreply.github.com>
2024-02-20 11:01:02 +0000

    asdasdasd

--- carstack.py
+++ carstack.py
@@ -1,31 +1,37 @@
-cars = [['', None], ['','0'], ['','1'], ['','2'], ['','3'], ['','4']]
+cars = [['', None], ['','0'], ['','1'], ['','2'], ['','3'], ['', 4]]
 top = -1
 data = 0
+size = 0
 pointer = 1
 def push(item):
     global top
+    global size
     top = top + 1
+    size = size + 1
     cars[top][data] = item
     print(cars)
-
 def pop():
-    cars.pop()
+    global top
+    global size
+    currentNodeData = cars[top][data]
+    top = top - 1
+    size = size - 1
     print(cars)
+    return currentNodeData
 def isFull():
-    if len(cars) == 6:
+    if cars[top][pointer] == 4:
         return True
     else:
         return False
 def isEmpty():
-    if len(cars) == 0:
+    if top == -1:
         return True
     else:
         return False
-def size():
-    return len(cars)
+def howBig():
+    return size
 def peek():
-    temp = len(cars)
-    return cars[temp]
+    return cars[top][data]
 
 itemToPush = input('what would you like to add to the list? ')
 push(itemToPush)
